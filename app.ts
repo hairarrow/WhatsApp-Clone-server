2a053950e67276cdfc93b04d638d90b4d89ae8c1
DAB0mB <emanor6@gmail.com>
2019-04-07 16:51:34 +0800

    Step 10.2: Add /sign-in route

--- app.ts
+++ app.ts
@@ -2,7 +2,10 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import express from 'express'
-import { origin } from './env'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { users } from './db'
+import { expiration, origin, secret } from './env'
 
 export const app = express()
 
@@ -12,4 +15,25 @@ app.use(cookieParser())
 
 app.get('/_ping', (req, res) => {
   res.send('pong')
+})
+
+app.post('/sign-in', (req, res) => {
+  const { username, password } = req.body
+
+  const user = users.find(u => u.username === username)
+
+  if (!user) {
+    return res.status(404).send('user not found')
+  }
+
+  const passwordsMatch = bcrypt.compareSync(password, user.password)
+
+  if (!passwordsMatch) {
+    return res.status(400).send('password is incorrect')
+  }
+
+  const authToken = jwt.sign(username, secret)
+
+  res.cookie('authToken', authToken, { maxAge: expiration })
+  res.status(200).send({ id: user.id })
 })
