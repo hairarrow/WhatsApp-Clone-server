9394de4dfc2bd14866f87d89bae181836cfb12d1
DAB0mB <emanor6@gmail.com>
2019-04-10 14:52:53 +0800

    Step 10.5: Add Query.me

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -65,6 +65,10 @@ const resolvers: Resolvers = {
   },
 
   Query: {
+    me(root, args, { currentUser }) {
+      return currentUser || null
+    },
+
     chats(root, args, { currentUser }) {
       if (!currentUser) return []
 
